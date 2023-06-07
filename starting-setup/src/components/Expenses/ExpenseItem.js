9a3221f88efed345a546e0541d8530f2d02ca418
vasuwebdeveloper <kasipurivasu@gmail.com>
2023-06-07 12:18:04 +0530

    react state hook

--- starting-setup/src/components/Expenses/ExpenseItem.js
+++ starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
+import {useState} from 'react'
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title); //resgistering the property.
+
+  const clickHandler = (title) => {
+    setTitle('updated!!!!');
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date = {props.date}/>
       </div>
       <div className = "expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className = "expense-item__price">{props.amount}</div>
       </div>
+      <button onClick = {clickHandler}>Change Title</button>
     </Card>
   );
 }
