2bbda3c68b50ec7cad2966e130b5601e555f63bd
vasuwebdeveloper <kasipurivasu@gmail.com>
2023-06-08 08:39:47 +0530

    expenses form changes

--- starting-setup/src/App.js
+++ starting-setup/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -28,7 +29,9 @@ const App = () => {
   ];
 
   return (
+    
    <div>
+    <NewExpense/>
     <Expenses items = {expenses}/>
    </div>
   );
