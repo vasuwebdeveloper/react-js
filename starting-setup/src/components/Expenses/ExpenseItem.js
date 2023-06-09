e2f73d612cb58e305d6a156a6d48e39024533983
vasuwebdeveloper <kasipurivasu@gmail.com>
2023-06-09 18:51:08 +0530

    updates to readme

--- starting-setup/src/components/Expenses/ExpenseItem.js
+++ starting-setup/src/components/Expenses/ExpenseItem.js
@@ -5,23 +5,15 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title); //registering the property.
-
-  const clickHandler = (title) => {
-    setTitle('updated!!!!');
-    console.log(title);
-  }
-
   return (
     <Card className="expense-item">
       <div>
         <ExpenseDate date = {props.date}/>
       </div>
       <div className = "expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className = "expense-item__price">{props.amount}</div>
       </div>
-      <button onClick = {clickHandler}>Change Title</button>
     </Card>
   );
 }
