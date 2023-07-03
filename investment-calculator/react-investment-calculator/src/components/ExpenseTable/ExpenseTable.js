b85d72a6cb68b758531759d83da2719cb1a61169
vasuwebdeveloper <kasipurivasu@gmail.com>
2023-07-04 00:31:50 +0530

    app changes

--- investment-calculator/react-investment-calculator/src/components/ExpenseTable/ExpenseTable.js
+++ investment-calculator/react-investment-calculator/src/components/ExpenseTable/ExpenseTable.js
@@ -1,34 +1,13 @@
-const ExpenseTable = ({ items }) => {
+const ExpenseTable = (props) => {
+  debugger;
   return (
-    <table className="result">
-      <thead>
         <tr>
-          <th>Year</th>
-          <th>Total Savings</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
+          <th>{props.year}</th>
+          <th>{props.totalSavings}</th>
+          <th>{props.yearlyInterest}</th>
+          <th>{props.totalInterest}</th>
+          <th>{props.yearlyContribution}</th>
         </tr>
-      </thead>
-      <tbody>
-        <tr>
-          {props.items.map((goal) => (
-            <CourseGoalItem
-              key={goal.id}
-              id={goal.id}
-              onDelete={props.onDeleteItem}
-            >
-              {goal.text}
-            </CourseGoalItem>
-          ))}
-          <td>YEAR NUMBER</td>
-          <td>TOTAL SAVINGS END OF YEAR</td>
-          <td>INTEREST GAINED IN YEAR</td>
-          <td>TOTAL INTEREST GAINED</td>
-          <td>TOTAL INVESTED CAPITAL</td>
-        </tr>
-      </tbody>
-    </table>
   );
 };
 
