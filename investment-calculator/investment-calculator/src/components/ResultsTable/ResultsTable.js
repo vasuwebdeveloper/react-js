e3e68966d45be0aaf41b09b663f5430b30dfddfd
vasuwebdeveloper <kasipurivasu@gmail.com>
2023-07-09 23:38:13 +0530

    render table results

--- investment-calculator/investment-calculator/src/components/ResultsTable/ResultsTable.js
+++ investment-calculator/investment-calculator/src/components/ResultsTable/ResultsTable.js
@@ -1,26 +1,37 @@
-const ResultsTable = () => {
-    return (
-        <table className="result">
-        <thead>
-          <tr>
-            <th>Year</th>
-            <th>Total Savings</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>YEAR NUMBER</td>
-            <td>TOTAL SAVINGS END OF YEAR</td>
-            <td>INTEREST GAINED IN YEAR</td>
-            <td>TOTAL INTEREST GAINED</td>
-            <td>TOTAL INVESTED CAPITAL</td>
-          </tr>
-        </tbody>
-      </table>
-    )
-}
+const ResultsTable = (props) => {
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+   
+  
+  return (
+    <table className="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.data.map((yearlyData) => {
+          return (<tr key = {yearlyData.year}>
+            <td>{yearlyData.year}</td>
+            <td>{formatter.format(yearlyData.savingsEndOfYear)}</td>
+            <td>{formatter.format(yearlyData.yearlyInterest)}</td>
+            <td>{formatter.format(yearlyData.savingsEndOfYear - props.initialInvestment - yearlyData.yearlyContribution * yearlyData.year) }</td>
+            <td>{formatter.format(props.initialInvestment + yearlyData.yearlyContribution * yearlyData.year)}</td>
+          </tr>)
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default ResultsTable;
